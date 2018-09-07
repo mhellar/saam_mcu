d05fb83246b47c23845309291c44bc5408b07334
Mark Hellar <mark.hellar@gmail.com>
2018-09-06 18:47:14 -0700

    clean

--- day2/codeDay2/bartpredict/index.js
+++
@@ -1,6 +0,0 @@
-var bart = require('bart').createClient({ "interval": 3000 });
-
-bart.on('dbrk', function(estimates) {
-    // console.log(estimates);
-    console.log("next train is headed " + estimates[0].direction + " in " + estimates[0].minutes + " destination is " + estimates[0].destination);
-});
